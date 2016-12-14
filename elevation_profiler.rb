b71d1e4711e9792b2a3a807c0d4d56c0a0e4305c
Dawid Janczak <dawid.janczak@gmail.com>
2016-12-14 18:52:46 +0100

    Add Sinatra::Reloader module and json

--- elevation_profiler.rb
+++ elevation_profiler.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class ElevationProfiler < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     haml :home
+  end
+
+  get '/calculate_route' do
+    content_type :json
+
+    { r: 4 }.to_json
   end
 end
